a1b4e0a9d05b8be08917fcda21ce7512139e5d4d
Alexsander <Kopul@ua.fm>
2019-05-04 13:28:43 +0300

    Add function Check leter have symbol right and left or not

--- coderByte.js
+++ coderByte.js
@@ -80,7 +80,7 @@ function SimpleAdding(num) {
 
 //Letter Capitalize
 /**
- * @return {string[]}
+ * @return {string}
  */
 function LetterCapitalize(str) {
 
@@ -88,7 +88,37 @@ function LetterCapitalize(str) {
     let mapArr = arr.map(function (item) {
         return item.charAt(0).toUpperCase() + item.slice(1);
     });
-    let result = mapArr.join(" ");
+    return mapArr.join(" ");
+}
 
-    return result;
+//Simple Symbols Results
+/**
+ * @return {boolean}
+ */
+function SimpleSymbols(str) {
+
+    // pad the strings so that if a character exists at the beginning
+    // of the string for example, we don't get on out-of-bounds error by
+    // trying to get the character before it
+    str = '=' + str + '=';
+
+    // loop through entire string
+    for (let i = 0; i < str.length; i++) {
+
+        // check to see if current character is an alphabetic character
+        // by using a simple case-insensitive regex pattern
+        if (str[i].match(/[a-z]/i) !== null) {
+
+            // check to see if a + symbol is to the left and right
+            // if not, then we know this string is not valid
+            if (str[i-1] !== '+' || str[i+1] !== '+') {
+                return false;
+            }
+
+        }
+
+    }
+
+    return true;
+
 }
