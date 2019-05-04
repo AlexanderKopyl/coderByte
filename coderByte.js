68d696db4dc0c3072e9e81a6fa908a689d252401
Alexsander <Kopul@ua.fm>
2019-05-04 14:17:24 +0300

    Add function sortChar

--- coderByte.js
+++ coderByte.js
@@ -122,3 +122,61 @@ function SimpleSymbols(str) {
     return true;
 
 }
+//Time Convert
+/**
+ * @return {string}
+ */
+function TimeConvert(num) {
+
+    // to get the hours, we divide num by 60 and round it down
+    // e.g. 61 / 60 = 1 hour
+    // e.g. 43 / 60 = 0 hours
+    var hours = Math.floor(num / 60);
+
+    // to get the minutes, now we use the remainder that we discarded above
+    // the modulo operation is represented by the % symbol
+    // e.g. 61 % 60 = 1 minute
+    // e.g. 43 % 60 = 43 minutes
+    var minutes = num % 60;
+
+    // combine the hours and minutes
+    return hours + ':' + minutes;
+
+
+}
+// SortCharecter
+/**
+ * @return {string}
+ */
+// function AlphabetSoup(str) {
+//
+//     let arr = [];
+//
+//     var string = "";
+//
+//     for(let i = 0; i < str.length; i++){
+//        arr.push(str[i].charCodeAt());
+//     }
+//     arr.sort();
+//
+//     arr.forEach(function (item) {
+//         string += String.fromCharCode(item);
+//     });
+//
+//     // code goes here
+//     return string;
+//
+// }
+
+function AlphabetSoup(str) {
+
+    // convert the string into an array of characters
+    var chars = str.split("");
+
+    // sort the array in alphabetical order
+    var sorted = chars.sort();
+
+    // return the newly sorted string
+    return sorted.join("");
+
+}
